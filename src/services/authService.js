dd0b0139c55b14e82653cc1638a1e2e8a6e93bd3
gildevson <gilsonfonseca92@gmail.com>
2024-10-30 20:08:58 -0300

    Sua mensagem de commit

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 export const login = async (email, password) => {
     try {
-        const response = await fetch('http://localhost:3000/auth/login', {
+        const response = await fetch('https://backend-bank.vercel.app/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
