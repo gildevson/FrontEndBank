2b0a57e58da910fb9282a6bee758008aefb74212
gildevson <gilsonfonseca92@gmail.com>
2024-10-31 23:04:02 -0300

    adicionando no horario de backend com dominio cem percento login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
         setLoading(true);
 
         try {
-            const response = await fetch('https://backend-bank.vercel.app/auth/login', {
+            const response = await fetch('https://bakend-bank.vercel.app/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
